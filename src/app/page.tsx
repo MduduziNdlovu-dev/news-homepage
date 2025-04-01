a83dc2972ef9e6c5c7e320c45a2bbba6c83912e1
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-04-01 21:31:47 +0200

    Add Navbar, Header, News, and Rankings components to Home page layout

--- src/app/page.tsx
+++ src/app/page.tsx
@@ -1,8 +1,25 @@
-import { Box } from '@mui/material';
+import { Box, Grid } from '@mui/material';
 import React from 'react';
+import Navbar from '../../components/Navbar';
+import Header from '../../components/Header';
+import News from '../../components/News';
+import Rankings from '../../components/Rankings';
 
 export default function Home() {
   return (
-    <Box></Box>
+    <Box>
+      <Navbar/>
+      <Grid container >
+        <Grid size={8} >
+          <Header/>
+        </Grid>
+        <Grid size={4} >
+          <News/>
+        </Grid>
+        <Grid size={12}>
+          <Rankings/>
+        </Grid>
+      </Grid>
+    </Box>
   );
 }
