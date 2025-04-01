3336d00f3614d140cfce72cfb3f68c1a53fe4e7a
Mduduzi-Ndlovu1 <129621544+Mduduzi-Ndlovu1@users.noreply.github.com>
2025-04-01 22:06:16 +0200

    Add Navbar component and new SVG assets; remove unused SVG files

--- components/Navbar.tsx
+++ components/Navbar.tsx
@@ -1,9 +1,65 @@
-import React from 'react'
+'use client'
+import React, { useState } from "react";
+import { AppBar, Toolbar, Typography, IconButton, Menu, MenuItem, Box } from "@mui/material";
+import MenuIcon from "@mui/icons-material/Menu";
 
-const Navbar = () => {
+const Navbar: React.FC = () => {
+  const [anchorEl, setAnchorEl] = useState<null | HTMLElement>(null);
+
+  const handleMenuOpen = (event: React.MouseEvent<HTMLButtonElement>) => {
+    setAnchorEl(event.currentTarget);
+  };
+
+  const handleMenuClose = () => {
+    setAnchorEl(null);
+  };
+
+  const menuItems = ["Home", "About", "Services", "Portfolio", "Contact"];
+
   return (
-    <div>Navbar</div>
-  )
-}
+    <AppBar position="static" sx={{ backgroundColor: "#333" }}>
+      <Toolbar>
+        {/* Logo */}
+        <Typography variant="h6" sx={{ flexGrow: 1 }}>
+          Logo
+        </Typography>
 
-export default Navbar
+        {/* Desktop Menu */}
+        <Box sx={{ display: { xs: "none", md: "flex" } }}>
+          {menuItems.map((item, index) => (
+            <Typography key={index} sx={{ margin: "0 15px", cursor: "pointer" }}>
+              {item}
+            </Typography>
+          ))}
+        </Box>
+
+        {/* Mobile Menu Button */}
+        <IconButton
+          edge="end"
+          color="inherit"
+          aria-label="menu"
+          sx={{ display: { xs: "block", md: "none" } }}
+          onClick={handleMenuOpen}
+        >
+          <MenuIcon />
+        </IconButton>
+
+        {/* Mobile Menu */}
+        <Menu
+          anchorEl={anchorEl}
+          open={Boolean(anchorEl)}
+          onClose={handleMenuClose}
+          sx={{ display: { xs: "block", md: "none" } }}
+        >
+          {menuItems.map((item, index) => (
+            <MenuItem key={index} onClick={handleMenuClose}>
+              {item}
+            </MenuItem>
+          ))}
+        </Menu>
+      </Toolbar>
+    </AppBar>
+  );
+};
+
+export default Navbar;
